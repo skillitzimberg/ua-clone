aa84455f5710fcfc29cb3d0d8b9e3e68ae786917
Scott Bergler <example@example.com>
2019-02-01 08:41:20 -0800

    add item.service.ts.

---
+++ src/app/item.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
+import { Item } from './item.model';
+
+@Injectable()
+export class ItemService {
+  items: FirebaseListObservable<any[]>;
+
+  constructor(
+    private database: AngularFireDatabase
+  ) {
+    this.items = database.list('items');
+  }
+
+  getItems() {
+    return this.items;
+  }
+}
