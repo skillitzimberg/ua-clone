32c86c54ab9e33a6bca1a59438f4ac655a32ab11
Scott Bergler <example@example.com>
2019-02-01 10:48:35 -0800

    pass data from store component to item-card component.

--- src/app/item-card/item-card.component.ts
+++ src/app/item-card/item-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Item } from '../item.model';
 
 @Component({
   selector: 'app-item-card',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item-card.component.css']
 })
 export class ItemCardComponent implements OnInit {
-
+  @Input() items: Item[];
   constructor() { }
 
   ngOnInit() {
