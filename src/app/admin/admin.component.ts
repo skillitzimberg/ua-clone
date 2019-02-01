44e220cb9fd088e1b42ca5bbccd1ccccc7bb63c4
Scott Bergler <example@example.com>
2019-02-01 13:45:37 -0800

    add submitForm() method to Admin.

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Item } from '../item.model';
 
 @Component({
   selector: 'app-admin',
@@ -11,5 +12,22 @@ export class AdminComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  submitForm(
+    gender: string,
+    category: string,
+    subcategories: string[],
+    name: string,
+    description: string,
+    price: number,
+    newArrival: boolean,
+    bestSeller: boolean,
+    outlet: boolean
+  ) {
+    let newItem: Item = new Item( gender, category, subcategories, name, description, price, newArrival, bestSeller, outlet );
+
+    console.log(newItem);
+  }
+
 
 }
