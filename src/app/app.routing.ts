395010ad127853fca21bf4f57ace7a3e97458517
Scott Bergler <example@example.com>
2019-02-01 09:39:36 -0800

    refactor homepage with routing and index component.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,12 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from '../app/app.component';
+import { IndexComponent } from './index/index.component';
 import { StoreComponent } from './store/store.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
-  { path: 'index', component: AppComponent },
+  { path: 'index', component: IndexComponent },
   { path: 'store', component: StoreComponent },
   { path: '',   redirectTo: '/index', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
