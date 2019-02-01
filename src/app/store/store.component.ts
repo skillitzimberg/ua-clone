06942478c92e051a311838012f801a5d4269e82f
Scott Bergler <example@example.com>
2019-02-01 08:56:58 -0800

    set up store.component.ts basics.

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FirebaseListObservable } from 'angularfire2/database';
+
+import { Item } from '../item.model';
+import { ItemService } from '../item.service';
+
 @Component({
   selector: 'app-store',
   templateUrl: './store.component.html',
-  styleUrls: ['./store.component.css']
+  styleUrls: ['./store.component.css'],
+  providers: [ ItemService ]
 })
-export class StoreComponent implements OnInit {
 
-  constructor() { }
+export class StoreComponent implements OnInit {
+  items;
+
+  constructor(
+    private itemService: ItemService
+  ) { }
 
   ngOnInit() {
+    this.itemService.getItems()
+    .subscribe((dataFromItemService) => {
+      this.items=dataFromItemService;
+    });
   }
 
 }
