3c60324e732ef00911a4ffe0e15f23d94824e62b
Scott Bergler <example@example.com>
2019-02-01 15:45:45 -0800

    add current state of work with searching.

--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -1,14 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchService } from '../search.service';
+import { Subject } from 'rxjs/Subject';
 
 @Component({
   selector: 'app-main-nav',
   templateUrl: './main-nav.component.html',
-  styleUrls: ['./main-nav.component.css']
+  styleUrls: ['./main-nav.component.css'],
+  providers: [ SearchService ]
 })
 export class MainNavComponent implements OnInit {
+  results: Object;
+  searchTerm$ = new Subject<string>();
 
-  constructor() { }
-
+  constructor( private searchService: SearchService ) {
+    this.searchService.search(this.searchTerm$)
+      .subscribe(results => {
+        this.results = results.results;
+      });
+  }
   ngOnInit() {
   }
 
