774e0cb0b1a568a5e0c87531f06a4c0d4389474b
Scott Bergler <example@example.com>
2019-02-01 16:09:03 -0800

    comment out search.service-related code to get the project back to a working state.

--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -1,24 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { SearchService } from '../search.service';
-import { Subject } from 'rxjs/Subject';
 
 @Component({
   selector: 'app-main-nav',
   templateUrl: './main-nav.component.html',
-  styleUrls: ['./main-nav.component.css'],
-  providers: [ SearchService, HttpClient ]
+  styleUrls: ['./main-nav.component.css']
 })
 export class MainNavComponent implements OnInit {
-  results: Object;
-  searchTerm$ = new Subject<string>();
 
-  constructor( private searchService: SearchService ) {
-    this.searchService.search(this.searchTerm$)
-      .subscribe(results => {
-        this.results = results.results;
-      });
-  }
+  constructor() { }
+
   ngOnInit() {
   }
 
--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -1,27 +1,27 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/debounceTime';
-import 'rxjs/add/operator/distinctUntilChanged';
-import 'rxjs/add/operator/switchMap';
-
-@Injectable()
-export class SearchService {
-  baseUrl: string = 'https://ua-clone.firebaseio.com';
-  queryUrl: string = '?search=';
-
-  constructor( private http: HttpClient ) { }
-
-  search(terms: Observable<string>) {
-    return terms.debounceTime(400)
-      .distinctUntilChanged()
-      .switchMap(term => this.searchEntries(term));
-  }
-
-  searchEntries(term) {
-    return this.http
-        .get(this.baseUrl + this.queryUrl + term)
-        .map(res => JSON.parse(res));
-  }
-}
+// import { Injectable } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+// import { Observable } from 'rxjs/Observable';
+// import 'rxjs/add/operator/map';
+// import 'rxjs/add/operator/debounceTime';
+// import 'rxjs/add/operator/distinctUntilChanged';
+// import 'rxjs/add/operator/switchMap';
+//
+// @Injectable()
+// export class SearchService {
+//   baseUrl: string = 'https://ua-clone.firebaseio.com';
+//   queryUrl: string = '?search=';
+//
+//   constructor( private http: HttpClient ) { }
+//
+//   search(terms: Observable<string>) {
+//     return terms.debounceTime(400)
+//       .distinctUntilChanged()
+//       .switchMap(term => this.searchEntries(term));
+//   }
+//
+//   searchEntries(term) {
+//     return this.http
+//         .get(this.baseUrl + this.queryUrl + term)
+//         .map(res => JSON.parse(res));
+//   }
+// }
